f8c743c73a4faa465f332d43adc254498c9dc59f
liar55 <ktc25a31e0017@edu.kyoto-tech.ac.jp>
2025-10-18 15:25:25 +0900

    fix: standardize quotation marks in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [tailwindcss(), react()],
-  base: command === 'serve' ? '/' : '/react-pokemon-zukan/',
+  base: command === "serve" ? "/" : "/react-pokemon-zukan/",
 }));
-
